f935ef945d5f31156b33d4eb1b6848dc9ef98b71
Dipen parmar <dipensavji@gmail.com>
2021-12-12 16:49:18 +0530

    wip sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Layout from './layout/Layout'
+
 function App() {
   return (
     <>
-      <div className='mx-auto w-[22rem] mt-10 p-10 px-16 bg-blue-200 shadow-xl'>
-        <span className='text-2xl'>React </span>
-        <span className='text-blue-800 '>Tailwind</span>
-      </div>
+      <Layout />
     </>
   )
 }
