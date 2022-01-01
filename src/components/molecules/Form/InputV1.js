d882446c9e97d460a9276cb09e06ac9f4f6e5fe5
Dipen parmar <dipensavji@gmail.com>
2022-01-01 14:04:07 +0530

    InputText wip

--- src/components/molecules/Form/InputV1.js
+++ src/components/molecules/Form/InputV1.js
@@ -2,39 +2,49 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import classNames from 'classnames'
 import React from 'react'
+// import { BiError } from 'react-icons/bi'
 
-function InputV1({
-  name,
-  className,
-  label,
-  placeholder,
-  value,
-  onChange,
-  onBlur,
-  onFocus,
-  error,
-  ...inputProps
-}) {
+function InputV1(
+  {
+    name,
+    label,
+    placeholder,
+    value,
+    onChange,
+    onBlur,
+    onFocus,
+    error,
+    classNames: {
+      container: containerClassName = '',
+      input: inputClassName = '',
+      label: labelClassName = '',
+      error: errorClassName = '',
+    } = {},
+    ...inputProps
+  },
+  inputRef,
+) {
+  const [_, setTest] = React.useState(false)
   return (
-    <div className={classNames('flex flex-col', className)}>
+    <fieldset className={classNames(containerClassName)}>
       <label
         className={classNames(
-          'block mb-1 text-gray-700',
-          error && 'text-red-400',
+          'flex items-center mb-1 text-gray-600 dark:text-gray-300',
+          // isError && 'text-red-400',
+          labelClassName,
         )}
-        htmlFor={name}
+        htmlFor="user_name"
+        onClick={() => setTest(!error)}
       >
         {label}
       </label>
       <input
-        {...inputProps}
-        onChange={onChange}
-        onBlur={onBlur}
-        name={name}
-        onFocus={onFocus}
-        value={value}
-        placeholder={placeholder}
-        className={classNames(
+        ref={inputRef}
+        autoComplete="name"
+        type="text"
+        name="username"
+        defaultValue={'dipen parmar12'}
+        className={classNames([
           'outline-none w-full ',
           'block rounded-lg border border-transparent text-gray-700',
           'py-2 px-4 bg-gray-100',
@@ -44,12 +54,21 @@ function InputV1({
           'transition duration-200 ease-in-out',
           error &&
             'bg-red-100/80 text-red-400  hover:bg-red-100/80 active:bg-red-100/80 focus:bg-red-100/80',
-        )}
+          inputClassName,
+        ])}
+        {...inputProps}
       />
       {error && (
-        <div className="px-1 text-sm italic text-red-400 ">{error}</div>
+        <div
+          className={classNames(
+            'text-red-500 text-sm font-medium',
+            errorClassName,
+          )}
+        >
+          {error}
+        </div>
       )}
-    </div>
+    </fieldset>
   )
 }
 export default React.forwardRef(InputV1)
--- src/pages/Users/AddModal.js
+++ src/pages/Users/AddModal.js
@@ -3,12 +3,10 @@
 import React from 'react'
 import classNames from 'classnames'
 import Button from '@/components/atoms/Button'
-import Random from '@/utils/faker/random'
 import ModalV1 from '@/components/molecules/Modal/ModalV1'
 import InputV1 from '@/components/molecules/Form/InputV1'
 
 export default function AddUser() {
-  const [isError, setTest] = React.useState(false)
   return (
     <>
       <div className="px-3 py-4 rounded shadow-md">
@@ -19,39 +17,21 @@ export default function AddUser() {
             type="text"
             name="username"
             defaultValue={'dipen parmar12'}
-            error={'This is error'}
+            classNames={{
+              container: 'flex-1',
+            }}
           />
-          <fieldset>
-            <label
-              className="block mb-1 text-gray-700"
-              htmlFor="user_name"
-              onClick={() => setTest(!isError)}
-            >
-              Error Name
-            </label>
-            <input
-              autoComplete="name"
-              type="text"
-              name="username"
-              defaultValue={'dipen parmar12'}
-              className={classNames([
-                'outline-none w-full ',
-                'block rounded-lg border border-transparent text-gray-700',
-                'py-2 px-4 bg-gray-100',
-                'hover:bg-white hover:border-blue-300 hover:shadow-outline-blue',
-                'active:bg-white',
-                'focus:bg-white focus:border focus:shadow-outline-blue focus:border-blue-300 focus:shadow-outline',
-                'transition duration-200 ease-in-out',
-                isError &&
-                  'bg-red-100/80 text-red-400  hover:bg-red-100/80 active:bg-red-100/80 focus:bg-red-100/80',
-              ])}
-            />
-            {isError && (
-              <div className="px-1 text-sm italic text-red-400 ">
-                Something went wrong Something
-              </div>
-            )}
-          </fieldset>
+          <InputV1
+            label="First Name"
+            autoComplete="name"
+            type="text"
+            name="username"
+            defaultValue={'dipen parmar12'}
+            error={'This is error'}
+            classNames={{
+              container: 'flex-1',
+            }}
+          />
         </div>
         <Button className={'my-3'}>Register</Button>
       </div>
