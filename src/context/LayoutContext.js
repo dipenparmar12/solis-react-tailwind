239db6996a3c6f62cf7b2987f55261eb6381b019
Dipen parmar <dipensavji@gmail.com>
2022-01-02 14:19:11 +0530

    wip

--- src/context/LayoutContext.js
+++ src/context/LayoutContext.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import useOnEscapeKeyDown from '@/hooks/useOnEscapeKeyDown'
-// import useOnOutsideClick from '@/hooks/useOnOutsideClick'
 import useLocalStorage from '@/hooks/useLocalStorage'
+import useOnEscapeKeyDown from '@/hooks/useOnEscapeKeyDown'
+import useOnOutsideClick from '@/hooks/useOnOutsideClick'
 
 const LayoutContext = React.createContext(null)
 export default function LayoutProvider({ children }) {
@@ -14,7 +14,7 @@ export default function LayoutProvider({ children }) {
   const [isSidebarVisible, setIsSidebarVisible] = React.useState(false)
   const handleUnVisibleSideBar = () => setIsSidebarVisible(false)
   const sidebarRef = React.useRef()
-  // useOnOutsideClick(sidebarRef, isSidebarVisible, handleUnVisibleSideBar)
+  useOnOutsideClick(sidebarRef, isSidebarVisible, handleUnVisibleSideBar)
   useOnEscapeKeyDown(isSidebarVisible, handleUnVisibleSideBar)
 
   // eslint-disable-next-line react/jsx-no-constructed-context-values
